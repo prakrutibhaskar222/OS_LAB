bcb6e07c9fcaecd3e1bf426dec1123407373ace4
prakrutibhaskar222 <prakrutibhaskar.cs24@bmsce.ac.in>
2026-03-06 12:38:20 +0530

    Update os1.c

--- week1/os1.c
+++ week1/os1.c
@@ -1,77 +1,37 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-struct Process {
-    int pid;
-    int at;
-    int bt;
-    int ct;
-    int tat;
-    int wt;
-    bool done;
-};
 
 int main() {
-    int n;
+    int n, i;
+    int bt[20], wt[20], tat[20];
+    float avg_wt = 0, avg_tat = 0;
+
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    struct Process p[n];
-
-    for (int i = 0; i < n; i++) {
-        p[i].pid = i + 1;
-        printf("Enter Arrival Time and Burst Time for P%d: ", i + 1);
-        scanf("%d %d", &p[i].at, &p[i].bt);
-        p[i].done = false;
+    for(i = 0; i < n; i++) {
+        printf("Enter Burst Time for P%d: ", i+1);
+        scanf("%d", &bt[i]);
     }
 
-    int firstArrival = p[0].at;
-    for (int i = 1; i < n; i++) {
-        if (p[i].at < firstArrival) {
-            firstArrival = p[i].at;
-        }
-    }
-    printf("\nFirst arrival time: %d\n", firstArrival);
+    wt[0] = 0;
 
-    int completed = 0, currentTime = 0;
-    float totalTAT = 0, totalWT = 0;
-
-    while (completed != n) {
-        int idx = -1;
-        int minBT = 1e9;
-
-        for (int i = 0; i < n; i++) {
-            if (p[i].at <= currentTime && !p[i].done) {
-                if (p[i].bt < minBT) {
-                    minBT = p[i].bt;
-                    idx = i;
-                }
-            }
-        }
-
-        if (idx != -1) {
-            currentTime += p[idx].bt;
-            p[idx].ct = currentTime;
-            p[idx].tat = p[idx].ct - p[idx].at;
-            p[idx].wt = p[idx].tat - p[idx].bt;
-            p[idx].done = true;
-
-            totalTAT += p[idx].tat;
-            totalWT += p[idx].wt;
-            completed++;
-        } else {
-            currentTime++;
-        }
+    for(i = 1; i < n; i++) {
+        wt[i] = wt[i-1] + bt[i-1];
     }
 
-    printf("\nProcess\tAT\tBT\tCT\tTAT\tWT\n");
-    for (int i = 0; i < n; i++) {
-        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",
-               p[i].pid, p[i].at, p[i].bt, p[i].ct, p[i].tat, p[i].wt);
+    for(i = 0; i < n; i++) {
+        tat[i] = wt[i] + bt[i];
+        avg_wt += wt[i];
+        avg_tat += tat[i];
     }
 
-    printf("\nAverage Turnaround Time: %.2f", totalTAT / n);
-    printf("\nAverage Waiting Time: %.2f\n", totalWT / n);
+    printf("\nProcess\tBT\tWT\tTAT\n");
+    for(i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\n", i+1, bt[i], wt[i], tat[i]);
+    }
+
+    printf("\nAverage Waiting Time = %.2f", avg_wt/n);
+    printf("\nAverage Turnaround Time = %.2f", avg_tat/n);
 
     return 0;
 }
